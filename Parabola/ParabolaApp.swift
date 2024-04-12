1a52c96b36f1a22997f44b53b57b1927886238df
CozyBrian <dragonbrian33@gmail.com>
2024-04-12 11:09:32 +0000

    Initial Commit

---
+++ Parabola/ParabolaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParabolaApp.swift
+//  Parabola
+//
+//  Created by Brian Newton on 12/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ParabolaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
